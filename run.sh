f53c0b662ec31c73015231ae33e08416b0fe1dd1
adityaparkhi <theaditya140@gmail.com>
2020-05-25 00:56:05 -0400

    conf

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 nvidia-smi
 pip install -r requirements.txt
 python test_cuda.py
-python mnist.py --gpu | tee /artifacts/mnist.log
+python mnist.py --gpu | tee /artifacts/mnis_gpu.log
+python mnist.py | tee /artifacts/mnist_cpu.log
